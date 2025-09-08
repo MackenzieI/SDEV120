7c006a4f5a030c2481bd4568d45bbbaab42d7953
Mackenzie <84205474+MackenzieI@users.noreply.github.com>
2025-09-08 13:51:17 -0500

    Create helloSDEV120.py

---
+++ module3/helloSDEV120.py
@@ -0,0 +1,8 @@
+def greetClassmate():
+    # Welcomes user to SDEV 120, asks the
+    # user for their name and greets them
+    print("Welcome to SDEV 120 class!")
+    name = input("Enter your name: ")
+    print(f"Hello {name}")
+
+greetClassmate()
